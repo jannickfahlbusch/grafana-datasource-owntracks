427ab2cac407929fb3f362b3170ce7e5fc96ac08
Jannick Fahlbusch <git@jf-projects.de>
2020-10-25 21:06:25 +0100

    Add Variable Query Editor

--- src/module.ts
+++ src/module.ts
@@ -3,7 +3,9 @@ import { DataSource } from './DataSource';
 import { ConfigEditor } from './ConfigEditor';
 import { QueryEditor } from './QueryEditor';
 import { OwntracksQuery, OwntracksDatasourceOptions } from './types';
+import { VariableQueryEditor } from './VariableQueryEditor';
 
 export const plugin = new DataSourcePlugin<DataSource, OwntracksQuery, OwntracksDatasourceOptions>(DataSource)
   .setConfigEditor(ConfigEditor)
-  .setQueryEditor(QueryEditor);
+  .setQueryEditor(QueryEditor)
+  .setVariableQueryEditor(VariableQueryEditor);
